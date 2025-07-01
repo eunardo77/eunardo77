bfe9737ef8894a04fc3e610b91ac5955158e4bf3
eunardo77 <ndzzksolo@gmail.com>
2025-07-01 14:41:21 -0300

    check

--- sistema/serializers.py
+++ sistema/serializers.py
@@ -9,12 +9,19 @@ class ProdutoSerializer(serializers.ModelSerializer):
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
+    confirm_password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
+        fields = ['username', 'email', 'password', 'confirm_password']
+
+    def validate(self, data):
+        if data['password'] != data['confirm_password']:
+            raise serializers.ValidationError("As senhas não coincidem.")
+        return data
 
     def create(self, validated_data):
+        validated_data.pop('confirm_password')  # Remove antes de criar o usuário
         user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
